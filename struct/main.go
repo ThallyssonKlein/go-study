e5ed2ed2715952576b2b34c4e9c916bddba3becc
thallyssonklein <thallyssonklein@gmail.com>
2024-11-14 05:01:29 -0300

    refact: struct

--- struct/main.go
+++ struct/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
 
 type Circle struct {
 	radius float64
@@ -11,6 +16,16 @@ func (c Circle) Area() float64 {
 }
 
 func main(){
-	c := Circle{radius: 5}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the radius of the circle: ")
+	input, _ := reader.ReadString('\n')
+
+	radius, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		fmt.Println("Invalid input")
+		os.Exit(1)
+	}
+
+	c := Circle{radius: radius}
 	fmt.Println(c.Area())
 }
